f03f06618d71b49cd99abb308538b0a1b1a91ea5
prabha2616 <prabham8516@gmail.com>
2023-09-30 09:45:32 -0400

    adding logic to find correct word

--- app.js
+++ app.js
@@ -11,51 +11,70 @@ const listOfWords = [
     'chart',
     'chase',
     'cheap',
-    'check'
+    'check',
+    'rooms',
+    'sleek'
 ]
-
+function getRandomWord() {
+    let randomNo = Math.floor(Math.random() * listOfWords.length);
+    // console.log(randomNo);
+    return listOfWords[randomNo]
+}
 function main() {
     let letterBoxNo = 0;
-    let randomNo = Math.floor(Math.random() * listOfWords.length);
-    console.log(randomNo);
-    let guessingWord = listOfWords[randomNo]
+    // Get a random word from list of words
+    let guessingWord = getRandomWord();
+    // let guessingWord = 'rooms'
     console.log('guessing word', guessingWord.toUpperCase().split(''));
     const guessContainer = document.querySelectorAll('.letter-box')
-    console.log(guessContainer.length);
+    // console.log(guessContainer.length);
     const keyLetter = document.querySelectorAll('.keyboard-btn')
     let wordGuess = {
         word: [],
         position: []
     };
-    // Refresh page
-    document.querySelector('#refresh').addEventListener('click', (e) => {
+    function refreshPage() {
+        document.querySelectorAll('.keyboard-btn').forEach((btn) => btn.disabled = false)
         guessContainer.forEach((div) => {
             div.textContent = ''
             div.classList.remove('orange')
             div.classList.remove('green')
             div.classList.remove('gray')
             letterBoxNo = 0;
-            document.getElementById('refresh').style.display = 'none';
+            // document.getElementById('refresh').style.display = 'block';
         })
         keyLetter.forEach((key) => {
             key.classList.remove('gray');
         })
-        randomNo = Math.floor(Math.random() * listOfWords.length);
-        guessingWord = listOfWords[randomNo]
+        wordGuess.word = [];
+        wordGuess.position = [];
+        guessingWord = getRandomWord()
         console.log('guessing word', guessingWord.toUpperCase().split(''));
-    })
+    }
+    // Refresh page when refresh button clicked
+    document.querySelector('#refresh').addEventListener('click', (e) => refreshPage())
     // Adding event listener for keyboard buttons
     keyLetter.forEach((char) => char.addEventListener('click', (e) => {
-        console.log(letterBoxNo);
+        // console.log(letterBoxNo);
         const letter = e.target.id
 
         if (letter !== 'delete' && letter !== 'enter') {
-            guessContainer[letterBoxNo].textContent = letter;
-            guessContainer[letterBoxNo].classList.add('gray')
-            letterBoxNo += 1;
-            wordGuess.word.push(letter)
-            wordGuess.position.push(letterBoxNo)
+            if (wordGuess.word.length < 5) {
+                // console.log(wordGuess.word.length);
+                guessContainer[letterBoxNo].textContent = letter;
+                // guessContainer[letterBoxNo].classList.add('gray')
+                // disable letter from keyboard if occurrence is one time
+                let noOccurrence = [...guessingWord.matchAll(new RegExp(letter, 'gi'))].map(a => a.index)
+                if (noOccurrence.length === 1) {
+                    // console.log(`no of occurrence of letter ${letter} is 1`);
+                    document.getElementById(letter).disabled = true
+                }
+                letterBoxNo += 1;
+                wordGuess.word.push(letter)
+                wordGuess.position.push(letterBoxNo)
+            }
             console.log(wordGuess.word);
+
         }
         if (letter === 'delete') {
             guessContainer[letterBoxNo - 1].textContent = '';
@@ -65,51 +84,69 @@ function main() {
             wordGuess.position.pop()
         }
         if (letter === 'enter') {
-            console.log('first guess', wordGuess.word.join(''));
-            console.log('first guess position', wordGuess.position);
-
+            console.log('Guess', wordGuess.word.join(''));
+            // console.log('Guess position', wordGuess.position);
+            document.querySelectorAll('.keyboard-btn').forEach((btn) => btn.disabled = false)
             if (wordGuess.word.length < 5) {
                 console.log('Guess a 5 letters word');
                 return
             }
-            let answer = guessingWord.toUpperCase().split('')
-            // if letter not found in answer
-            for (let i = 0; i < wordGuess.word.length; i++) {
-                let guessLetterIndex = answer.indexOf(wordGuess.word[i])
-                console.log('guess letter index',guessLetterIndex);
-                if (guessLetterIndex === -1) {
-                    console.log(wordGuess.word[i])
-                    document.getElementById(wordGuess.word[i]).classList.add('gray')
-                }
-                if (guessLetterIndex !== -1) {
-                    if(guessLetterIndex === i) {
-                        console.log('same position as answer for ', wordGuess.word[i]);
-                    } else {
-                        console.log('different position as answer for ', wordGuess.word[i]);
-                    }
-                }
-            }
-            
-            for (let i = 0; i < answer.length; i++) {
-                console.log('inside for loop', wordGuess.word.includes(answer[i]));
 
-                // if letter found in answer
-                if (wordGuess.word.includes(answer[i])) {
-                    // const charPosition = wordGuess.word.indexOf(answer[i].toUpperCase());
-                    const letterBoxPos = wordGuess.position[wordGuess.word.indexOf(answer[i])];
-                    // document.getElementById(letterBoxPos).style.backgroundColor = 'orange';
-                    document.getElementById(letterBoxPos).classList.remove('gray');
-                    document.getElementById(letterBoxPos).classList.add('orange');
-                    console.log(`first guess has ${answer[i]} at position ${wordGuess.word.indexOf(answer[i])} i = ${i}`);
-                    console.log(`letter-box position ${wordGuess.position[wordGuess.word.indexOf(answer[i])]}`);
-                    if (wordGuess.word.indexOf(answer[i]) == i) {
-                        console.log(`position of ${answer[i]} is same`);
-                        // document.getElementById(letterBoxPos).style.backgroundColor = 'green';
-                        document.getElementById(letterBoxPos).classList.remove('orange');
-                        document.getElementById(letterBoxPos).classList.add('green');
+            // let answer = guessingWord.toUpperCase().split('')
+            var letterColor = '';
+            // Iterate thru guess word 
+            for (let i = 0; i < wordGuess.word.length; i++) {
+                let guessLetter = wordGuess.word[i]
+                let letterBoxPos = wordGuess.position[i]
+                // console.log('letter box position', letterBoxPos);
+                // console.log(document.getElementById(letterBoxPos));
+                let guessLetterIndex = i;
+                const arrayOfIndexes = [...guessingWord.matchAll(new RegExp(guessLetter, 'gi'))].map(a => a.index);
+                // console.log(arrayOfIndexes);
+                // console.log([...guessingWord.matchAll(new RegExp(guessLetter, 'gi'))]);
+
+                // Check for letter not found
+                if (arrayOfIndexes.length === 0) {
+                    console.log(`${guessLetter} not found in answer`);
+                    letterColor = 'gray'
+                    document.getElementById(guessLetter.toUpperCase()).classList.add('gray')
+                }
+                // check for one time occurrance of letter
+                else if (arrayOfIndexes.length === 1) {
+                    if (guessLetterIndex === arrayOfIndexes[0]) {
+                        console.log(`${guessLetter} is at same position as answer`);
+                        letterColor = 'green';
+                    } else {
+                        
+                        console.log(`${guessLetter} is at different position as answer`);
+                        letterColor = 'orange';
                     }
                 }
+
+                // check for more than one occurrance of letter
+                else if (arrayOfIndexes.length > 1) {
+                    isAtSamePosition = false;
+                    let tempIndex = 0
+                    for (let i = 0; i < arrayOfIndexes.length; i++) {
+                        if (guessLetterIndex === arrayOfIndexes[i]) {
+                            isAtSamePosition = true
+                            tempIndex = guessLetterIndex
+                        }
+                    }
+                    if (isAtSamePosition) {
+                        console.log(`${guessLetter} at ${tempIndex} same as answer`);
+                        letterColor = 'green';
+                    } else {
+                        console.log(`${guessLetter} is at different position as answer`);
+                        letterColor = 'orange'
+                    }
+                    tempIndex = 0
+                }
+                // color the letter according to position is correct or not
+                console.log('letter color', letterColor);
+                document.getElementById(letterBoxPos).classList.add(letterColor);
             }
+            //All letters found 
             if (wordGuess.word.join('').toLowerCase() == guessingWord) {
                 console.log('You win');
                 document.getElementById('refresh').style.display = 'block';
@@ -118,13 +155,16 @@ function main() {
                 wordGuess.position = [];
                 return
             }
+            if (letterBoxNo > guessContainer.length - 1) {
+                console.log('End of guesses');
+                document.getElementById('refresh').style.display = 'block';
+                // refreshPage()
+                return
+            }
             wordGuess.word = [];
             wordGuess.position = [];
         }
-        if (letterBoxNo > guessContainer.length - 1) {
-            console.log('End of guesses');
-            return
-        }
+
     }))
 }
 main()
